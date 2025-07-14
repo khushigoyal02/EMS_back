10f12c2ac0ebf23d102cfd4233a694e574d855b4
khushigoyal11 <khushigoyal1028@gmail.com>
2025-07-14 11:14:31 +0530

    SAK2

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n'));
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 } else {
   serviceAccount = require("./serviceAccountKey.json"); // fallback for local dev
 }
